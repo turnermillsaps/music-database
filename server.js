f4d9c9bbbf987bbbad0cbe3ab72f6c0988af5e85
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-04-25 21:52:30 -0400

    Added the prompt to start the whole program in server.js, not tested yet

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const album = require('./create_album');
 const track = require('./create_track');
 
 // Main function to prompt user for all values in the database
+artist.newArtist();
+album.newAlbum();
+track.newTrack();
+
+prompt('Would you like to add another artist?')
+    .then((val) => {
+        if (val.toLowerCase() == 'yes') {
+            artist.newArtist();
+            album.newAlbum();
+            track.newTrack();
+        } else {
+            console.log('Thank you.');
+            prompt.done();
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+    })
